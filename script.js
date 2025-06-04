2b50a130b2ec84943cb069afd9307f505a8c4ae4
fthuggy <179480003+fthuggy@users.noreply.github.com>
2025-06-04 12:05:27 +0200

    added some small assets

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ window.addEventListener("DOMContentLoaded", () => {
   soffa.addEventListener("model-loaded", () => {
     soffa.object3D.traverse((node) => {
       if (node.isMesh && node.material) {
-        node.material.color.set("#e9e8e2");
+        node.material.color.set("#e9e8");
       }
     });
   });
