2cf0f3c03e2d742bc4d23d42af738067541f80f9
fthuggy <179480003+fthuggy@users.noreply.github.com>
2025-06-09 21:15:37 +0200

    added music

--- script.js
+++ script.js
@@ -3,9 +3,36 @@ window.addEventListener("DOMContentLoaded", () => {
   soffa.addEventListener("model-loaded", () => {
     soffa.object3D.traverse((node) => {
       if (node.isMesh && node.material) {
-        node.material.color.set("#e9e888");
+        node.material.color.set("#DE1F1F");
       }
     });
+  });
+
+  // Proximity sound-component
+  AFRAME.registerComponent("proximity-sound", {
+    schema: {
+      target: { type: "selector" },
+      threshold: { type: "number", default: 5 },
+    },
+    tick: function () {
+      const player = this.data.target || this.el.sceneEl.camera.el;
+      const soundEntity = this.el;
+      const distance = soundEntity.object3D.position.distanceTo(
+        player.object3D.position
+      );
+
+      if (
+        distance < this.data.threshold &&
+        !soundEntity.components.sound.isPlaying
+      ) {
+        soundEntity.components.sound.playSound();
+      } else if (
+        distance >= this.data.threshold &&
+        soundEntity.components.sound.isPlaying
+      ) {
+        soundEntity.components.sound.stopSound();
+      }
+    },
   });
 });
 
